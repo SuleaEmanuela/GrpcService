1a2c95673659b2c42baae62b6b52c26134e7e289
Ema <emanuelaflorentinasulea@gmail.com>
2021-03-23 08:03:37 +0200

    Add project files.

---
+++ GrpcService/DataAccess/PersonOperations.cs
@@ -0,0 +1,65 @@
+﻿using PersonService.Protos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonService.DataAccess
+{
+    public class PersonOperations
+    {
+        private readonly string filePath = "./Resources/people.txt";
+
+        public bool AddPerson(Person person)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine(person.Name);
+                sw.WriteLine(person.Gender);
+                sw.WriteLine(person.Age);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            return true;
+
+        }
+        public List<Person> GetPeople()
+        {
+            var people = new List<Person>();
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+                while (line!=null)
+                {
+                   string[] words = line.Split(' ');
+                    people.Add(new Person() { Name = words[0], Gender = words[1], Age = Int32.Parse(words[2]) });
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            return people;
+        }
+
+
+    }
+}
---
+++ GrpcService/Services/PersonRegistrationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using PersonService.Protos;
+using PersonService.DataAccess;
+using Microsoft.Extensions.Logging;
+
+namespace PersonRegistrationService.Services
+{
+
+    public class PersonRegistrationService : PersonRegistration.PersonRegistrationBase
+    {
+        private readonly PersonOperations personOperations = new PersonOperations();
+        private readonly ILogger<PersonRegistrationService> _logger;
+        public PersonRegistrationService(ILogger<PersonRegistrationService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<AddPersonResponse> AddPerson(AddPersonRequest request, ServerCallContext context)
+        {
+            var person = request.Person;
+            var people = personOperations.AddPerson(person);
+
+            _logger.Log(LogLevel.Information, "Added person: " + person.Name);
+            return Task.FromResult(new AddPersonResponse() { Status = AddPersonResponse.Types.Status.Success });
+        }
+
+        public override Task GetAllPersons(Empty request, IServerStreamWriter<Person> responseStream, ServerCallContext context)
+        {
+            return base.GetAllPersons(request, responseStream, context);
+        }
+
+    }
+}
